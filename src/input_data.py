b990096bb9e64aecfd7a8c63cf58e94c4c47a89a
Asiderr <norkam41@gmail.com>
2019-11-03 21:16:16 +0100

    src/test_input_data.py: Add TestInputData class, add test case for input src/test_input_data.py: Add InputData class, add default input values src/__init__.py: Add init file for folder

---
+++ src/input_data.py
@@ -0,0 +1,7 @@
+class InputData:
+    def __init__(self):
+        self.nominal_power = 400
+        self.voltage = 230
+        self.frequency = 50
+        self.poles_number = 3
+        self.synchronous_speed = 1000
