87b12ce56c3f9a02cbceece8015c52ba7150d7c2
Asiderr <norbert.kaminski@3mdeb.com>
2019-11-11 21:48:58 +0100

    src/input_data.py: Add nominal calculations methods

--- src/input_data.py
+++ src/input_data.py
@@ -5,3 +5,48 @@ class InputData:
         self.frequency = 50
         self.poles_number = 3
         self.synchronous_speed = 1000
+        self.efficiency = 0.8
+        self.power_coef = 0.8
+
+        check = input("Do you want to input your data? (y/n) \n")
+        if check == 'y':
+            temp = input("Enter nominal power value: \n")
+            while  str.isnumeric(temp) is False:
+                temp = input("Wrong value!! Enter the numeric value: \n")
+            self.nominal_power = int(temp)
+
+            temp = input("Enter nominal voltage value: \n")
+            while  str.isnumeric(temp) is False:
+                temp = input("Wrong value!! Enter the numeric value: \n")
+            self.voltage = int(temp)
+
+            temp = input("Enter frequency value: \n")
+            while  str.isnumeric(temp) is False:
+                temp = input("Wrong value!! Enter the numeric value: \n")
+            self.frequency = int(temp)
+            
+            temp = input("Enter poles number: \n")
+            while  str.isnumeric(temp) is False:
+                temp = input("Wrong value!! Enter the numeric value: \n")
+            self.poles_number = int(temp)
+
+            temp = input("Enter synchronous speed value \n")
+            while str.isnumeric(temp) is False:
+                temp = input("Wrong value!! Enter the numeric value: \n")
+            self.synchronous_speed = int(temp)
+    
+    def rotation_inducted_voltage_coefficient_calculation(self):
+        self.rotation_inducted_voltage_coefficient = 0.985-0.005*self.poles_number
+
+    def rotation_inducted_voltage_calculation(self):
+        self.rotation_inducted_voltage = self.rotation_inducted_voltage_coefficient*self.voltage
+    
+    def electric_power_calculation(self):
+        self.electric_power = self.nominal_power/self.efficiency
+
+    def nominal_current_calculation(self):
+        self.nominal_current = self.electric_power/(3*self.voltage*self.power_coef)
+
+    def inside_motor_power_calculation(self):
+        self.inside_motor_power = 3*self.nominal_current*self.rotation_inducted_voltage
+   
